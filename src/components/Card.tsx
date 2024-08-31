db8893309db9768e24e9525486eb15ad82b8f1c4
zawkindev <zawkindev@gmail.com>
2024-08-31 08:15:09 +0500

    fix: text overflow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, tags, date }) => {
   return (
-    <div className="w-[850px] bg-black-300 flex flex-col p-4 gap-4 rounded-lg hover:bg-black-50 hover:cursor-pointer">
+    <div className="w-[700px] bg-black-300 flex flex-col p-4 gap-4 rounded-lg hover:bg-black-50 hover:cursor-pointer">
       <div className="flex justify-between text-gray-400">
         <div className="flex gap-3">
           {tags.map((tag, index) => {
@@ -18,7 +18,7 @@ const Card: React.FC<CardProps> = ({ title, tags, date }) => {
         </div>
         <p>{date}</p>
       </div>
-      <p className="text-2xl">{title}</p>
+      <p className="text-2xl break-word">{title}</p>
     </div>
   );
 };
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ interface TagProps {
 
 const Tag: React.FC<TagProps> = ({ title }) => {
   return (
-    <div className="font-[JetBrainsMono] font-medium bg-black-100 px-1 rounded-sm hover:text-white hover:cursor-pointer hover:bg-slate-700">
+    <div className="font-[JetBrainsMono] font-medium bg-black-100 px-1 rounded-sm hover:text-white hover:cursor-pointer hover:bg-black-600">
       #{title}
     </div>
   );
